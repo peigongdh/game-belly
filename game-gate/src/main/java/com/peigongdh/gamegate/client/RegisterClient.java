90a1ccf4398b68110301cf3a51043ee2efe9ef4b
张沛 <zhangpei1@yixia.com>
2018-07-26 19:05:26 +0800

    add register client

--- game-gate/src/main/java/com/peigongdh/gamegate/client/RegisterClient.java
+++ game-gate/src/main/java/com/peigongdh/gamegate/client/RegisterClient.java
@@ -1,4 +1,38 @@
 package com.peigongdh.gamegate.client;
 
+import com.peigongdh.gamegate.handler.RegisterHandlerInitializer;
+import com.peigongdh.gamegate.server.WebSocketServer;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class RegisterClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketServer.class);
+
+    private Bootstrap bootstrap;
+
+    private boolean init() {
+        return true;
+    }
+
+    public void start() {
+        boolean initResult = init();
+        if (!initResult) {
+            logger.info("register client init error");
+        }
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            bootstrap = new Bootstrap();
+        } finally {
+            group.shutdownGracefully();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new RegisterHandlerInitializer());
+        }
+    }
+
 }
---
+++ game-gate/src/main/java/com/peigongdh/gamegate/handler/RegisterHandlerInitializer.java
@@ -0,0 +1,11 @@
+package com.peigongdh.gamegate.handler;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+public class RegisterHandlerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+
+    }
+}
